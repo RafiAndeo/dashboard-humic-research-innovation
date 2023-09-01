685d120e408ab03031d7103216e7a533bc20c089
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-09-01 13:51:27 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,74 +12,74 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class ReportController extends Controller
 {
-    public function cetak_pdf_hki()
-    {
-        $hki = hki::all();
+    // public function cetak_pdf_hki()
+    // {
+    //     $hki = hki::all();
 
-        $pdf = Pdf::loadview('hki_pdf', ['hki' => $hki]);
-        return $pdf->download('laporan-hki-pdf');
-    }
+    //     $pdf = Pdf::loadview('hki_pdf', ['hki' => $hki]);
+    //     return $pdf->download('laporan-hki-pdf');
+    // }
 
-    public function paper()
+    public function paper(Request $request)
     {
-        $paper = paper::all();
+        $paper = $request->paper;
         return view('report.report_paper', ['data' => $paper]);
         // $pdf = Pdf::loadview('report.report_paper', ['data' => $paper]);
         // return $pdf->download('laporan-paper-pdf');
         // return $pdf->stream();
     }
 
-    public function hki()
+    public function hki(Request $request)
     {
-        $hki = hki::all();
+        $hki = $request->hki;
         return view('report.report_hki', ['data' => $hki]);
         // $pdf = Pdf::loadview('report.report_paper', ['data' => $paper]);
         // return $pdf->download('laporan-paper-pdf');
         // return $pdf->stream();
     }
 
-    public function member()
+    public function member(Request $request)
     {
-        $member = member::all();
+        $member = $request->member;
         return view('report.report_member', ['data' => $member]);
     }
 
-    public function partner()
+    public function partner(Request $request)
     {
-        $partner = partner::all();
+        $partner = $request->partner;
         return view('report.report_partner', ['data' => $partner]);
     }
 
-    public function research()
+    public function research(Request $request)
     {
-        $research = research::all();
+        $research = $request->research;
         return view('report.report_research', ['data' => $research]);
         // $pdf = Pdf::loadview('report.report_paper', ['data' => $paper]);
         // return $pdf->download('laporan-paper-pdf');
         // return $pdf->stream();
     }
 
-    public function cetak_pdf_research()
-    {
-        $research = research::all();
+    // public function cetak_pdf_research()
+    // {
+    //     $research = research::all();
 
-        $pdf = Pdf::loadview('research_pdf', ['research' => $research]);
-        return $pdf->download('laporan-research-pdf');
-    }
+    //     $pdf = Pdf::loadview('research_pdf', ['research' => $research]);
+    //     return $pdf->download('laporan-research-pdf');
+    // }
 
-    public function cetak_pdf_member()
-    {
-        $member = member::all();
+    // public function cetak_pdf_member()
+    // {
+    //     $member = member::all();
 
-        $pdf = Pdf::loadview('member_pdf', ['member' => $member]);
-        return $pdf->download('laporan-member-pdf');
-    }
+    //     $pdf = Pdf::loadview('member_pdf', ['member' => $member]);
+    //     return $pdf->download('laporan-member-pdf');
+    // }
 
-    public function cetak_pdf_partner()
-    {
-        $partner = partner::all();
+    // public function cetak_pdf_partner()
+    // {
+    //     $partner = partner::all();
 
-        $pdf = Pdf::loadview('partner_pdf', ['partner' => $partner]);
-        return $pdf->download('laporan-partner-pdf');
-    }
+    //     $pdf = Pdf::loadview('partner_pdf', ['partner' => $partner]);
+    //     return $pdf->download('laporan-partner-pdf');
+    // }
 }
